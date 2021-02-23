14ea3f33a6af1b8c7603f0a03133c93a32993866
day1 <Sunnat1194@gmail.com>
2021-02-23 00:06:54 -0500

    add before and after method

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,4 +1,17 @@
 package com.cybertek.step_definitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    @Before
+    public void setUpScenario(){
+        System.out.println("Setting up browser with further details...");
+    }
+    @After
+    public void tearDownScenario(){
+        System.out.println("Closing browser");
+        System.out.println("Take a screenshot");
+    }
 }
